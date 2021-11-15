c34dce200775e61a8478ecf643cc61cd5e66a27a
CoolSnow <670696652@qq.com>
2021-11-15 16:03:57 +0800

    Update Layout.tsx

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -9,6 +9,7 @@ const Header = () => {
   return (
     <Head>
       <meta name="description" content="nextjs-quick-start" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
       <link rel="icon" href="/favicon.ico" />
       <title>nextjs-quick-start</title>
     </Head>
