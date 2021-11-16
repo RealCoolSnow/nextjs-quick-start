aa8113ff360287f9d4666814245fec1c06c46563
CoolSnow <670696652@qq.com>
2021-11-16 19:06:59 +0800

    fix: disable swc

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextTranslate = require('next-translate')
 module.exports = nextTranslate({
   reactStrictMode: true,
   productionBrowserSourceMaps: false, //生产环境source map
+  swcMinify: false,
   typescript: {
     //构建时忽略ts错误
     ignoreBuildErrors: false,
