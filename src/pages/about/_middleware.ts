02d54c08c263610e0b3054eae407068f614bea57
CoolSnow <670696652@qq.com>
2022-11-28 11:44:35 +0800

    Delete _middleware.ts

--- src/pages/about/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log('hello middleware')
-  return NextResponse.next()
-}
